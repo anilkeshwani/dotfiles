3e764b78b6e45688e8b9ac83d95c82684f7cc1f1
Anil Keshwani <anilkeshwani@hotmail.com>
2024-04-30 12:39:13 +0100

    Add install_dotfiles.sh script

---
+++ install_dotfiles.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+shopt -s dotglob
+
+DEFAULT_DOTFILES_DIR="${HOME}/_default_dotfiles"
+
+mk_default_dotfiles_dir() {
+    if [ ! -d "${DEFAULT_DOTFILES_DIR}" ]; then
+        mkdir "${DEFAULT_DOTFILES_DIR}"
+        echo "Created directory to store existing default dotfiles at ${DEFAULT_DOTFILES_DIR}"
+    fi
+}
+
+(
+    cd dotfiles
+    # move existing dotfiles in $HOME to DEFAULT_DOTFILES_DIR
+    for file in *; do
+        if [ ! -f "${HOME}/${file}" ]; then
+            mk_default_dotfiles_dir
+            mv "${file}" "${DEFAULT_DOTFILES_DIR}/${file}"
+            echo "Moved ${HOME}/${file} to ${DEFAULT_DOTFILES_DIR}/${file}"
+        fi
+    done
+    # create soft links in $HOME directory for repository dotfiles
+    for file in *; do
+        if [ -f "${file}" ]; then
+            ls -s "${file}" "${HOME}/${file}"
+        fi
+    done
+)
