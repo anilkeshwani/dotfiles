1692ca9635e496b4a38fa10805fbb33dfed39c1c
anilkeshwani <anilkeshwani@hotmail.com>
2025-04-02 16:39:39 +0100

    That's better

--- install_dotfiles.py
+++ install_dotfiles.py
@@ -15,24 +15,30 @@ LOGGER = logging.getLogger(__file__)
 DOTFILES_DIR = Path(__file__).parent / "dotfiles"
 DOTFILES_BACKUP_DIR = Path.home() / "dotfiles_backup"
 
-dotfiles: list[Path] = list(DOTFILES_DIR.iterdir())
-symlink_tgts: list[Path] = [Path.home() / dotfile.name for dotfile in dotfiles]
-symlink_tgts_exist: list[Path] = list(filter(lambda p: p.exists(), symlink_tgts))
 
-if symlink_tgts_exist:
-    LOGGER.info(f"Existing dotfiles found in {Path.home()}:\n" + "\n".join([str(p) for p in symlink_tgts_exist]))
-    try:
-        DOTFILES_BACKUP_DIR.mkdir(exist_ok=False)
-        LOGGER.info(f"Created directory to store existing dotfiles at {DOTFILES_BACKUP_DIR!s}")
-    except FileExistsError as e:
-        raise FileExistsError(f"Default backup directory for dotfiles exists at {DOTFILES_BACKUP_DIR}") from e
+def main():
+    dotfiles: list[Path] = list(DOTFILES_DIR.iterdir())
+    symlink_tgts: list[Path] = [Path.home() / dotfile.name for dotfile in dotfiles]
+    symlink_tgts_exist: list[Path] = list(filter(lambda p: p.exists(), symlink_tgts))
 
-for dotfile, symlink_src in zip(dotfiles, symlink_tgts):
-    if symlink_src.exists():
-        dst = DOTFILES_BACKUP_DIR / symlink_src.name
-        symlink_src.rename(dst)
-        LOGGER.info(f"Moved existing {symlink_src} to {dst}")
-    symlink_src.symlink_to(dotfile)
-    LOGGER.info(f"Symlinked {symlink_src} to {dotfile}")
+    if symlink_tgts_exist:
+        LOGGER.info(f"Existing dotfiles found in {Path.home()}:\n" + "\n".join([str(p) for p in symlink_tgts_exist]))
+        try:
+            DOTFILES_BACKUP_DIR.mkdir(exist_ok=False)
+            LOGGER.info(f"Created directory to store existing dotfiles at {DOTFILES_BACKUP_DIR!s}")
+        except FileExistsError as e:
+            raise FileExistsError(f"Default backup directory for dotfiles exists at {DOTFILES_BACKUP_DIR}") from e
 
-LOGGER.info("Installed dotfiles")
+    for dotfile, symlink_src in zip(dotfiles, symlink_tgts):
+        if symlink_src.exists():
+            dst = DOTFILES_BACKUP_DIR / symlink_src.name
+            symlink_src.rename(dst)
+            LOGGER.info(f"Moved existing {symlink_src} to {dst}")
+        symlink_src.symlink_to(dotfile)
+        LOGGER.info(f"Symlinked {symlink_src} to {dotfile}")
+
+    LOGGER.info("Installed dotfiles")
+
+
+if __name__ == "__main__":
+    main()
