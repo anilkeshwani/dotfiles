cfba388219f06b20a127d3792696c35a24afade8
anilkeshwani <anilkeshwani@hotmail.com>
2025-04-02 16:19:20 +0100

    Rename variables symlink_srcs* -> symlink_tgts* to reflect correct link nomenclature (see man ln; below)
    The ln utility creates a new directory entry (linked file) for the file name specified by target_file.  The target_file will be created with the same file modes as the source_file.  It is useful
         for maintaining multiple copies of a file in many places at once without using up storage for the “copies”; instead, a link “points” to the original copy.  There are two types of links; hard
         links and symbolic links.  How a link “points” to a file is one of the differences between a hard and symbolic link.

--- install_dotfiles.py
+++ install_dotfiles.py
@@ -15,18 +15,18 @@ DOTFILES_DIR = Path(__file__).parent / "dotfiles"
 DOTFILES_BACKUP_DIR = Path.home() / "dotfiles_backup"
 
 dotfiles: list[Path] = list(DOTFILES_DIR.iterdir())
-symlink_srcs: list[Path] = [Path.home() / dotfile.name for dotfile in dotfiles]
-existing_symlink_srcs: list[Path] = [p for p in symlink_srcs if p.exists(follow_symlinks=False)]
+symlink_tgts: list[Path] = [Path.home() / dotfile.name for dotfile in dotfiles]
+symlink_tgts_exist: list[Path] = [p for p in symlink_tgts if p.exists(follow_symlinks=False)]
 
-if existing_symlink_srcs:
-    LOGGER.info(f"Existing dotfiles found in {Path.home()}:\n" + "\n".join([str(p) for p in existing_symlink_srcs]))
+if symlink_tgts_exist:
+    LOGGER.info(f"Existing dotfiles found in {Path.home()}:\n" + "\n".join([str(p) for p in symlink_tgts_exist]))
     try:
         DOTFILES_BACKUP_DIR.mkdir(exist_ok=False)
         LOGGER.info(f"Created directory to store existing dotfiles at {DOTFILES_BACKUP_DIR!s}")
     except FileExistsError as e:
         raise FileExistsError(f"Default backup directory for dotfiles exists at {DOTFILES_BACKUP_DIR}") from e
 
-for dotfile, symlink_src in zip(dotfiles, symlink_srcs):
+for dotfile, symlink_src in zip(dotfiles, symlink_tgts):
     if symlink_src.exists():
         dst = DOTFILES_BACKUP_DIR / symlink_src.name
         symlink_src.rename(dst)
